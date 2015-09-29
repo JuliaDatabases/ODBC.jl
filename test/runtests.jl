6c857f2c1314601f56f9acb662e58ebdcafcccfe
bmharsha <bmharshajobs@gmail.com>
2015-09-29 15:18:34 +0530

    All queries manually tested

--- test/runtests.jl
+++ test/runtests.jl
@@ -61,8 +61,7 @@ function check_API_querymeta(q,print_file=0)
     return
   end
 end
-# Multi Statement is not supported by this wrapper, a multi-statement has been intentionally added
-#q = ["show databases;", "use dbtest;", "show tables;", "select * from employee;", "select count(*) from employee;", "show columns from employee;", "select id from employee group by id;", "select * from employee where mid = -1;","select count(*) from employee where mid = 0;", "select * from employee where mid = 2 limit 20;","select count(*) from employee; select count(*) from employee;","select a.id, b.id from employee a left outer join employee b on a.id = b.id where a.mid = 2 and b.mid = 0;"]
+# Many queries are invalid, they have been intentionally added
 q = ["show databases;", "use mysqltest1;", "show tables;", "select * from Employee;", "select count(*) from Employee;", "show columns from Employee;", "select ID from Employee group by ID;", "select * from Employee where Salary = -1;","select count(*) from Employee where OfficeNo > 0;", "select * from Employee where OfficeNo > 2 limit 2;","select count(*) from Employee; select count(*) from Employee;","select a.ID, b.ID from Employee a left outer join Employee b on a.ID = b.ID where a.OfficeNo > 2 and b.OfficeNo < 45;","select * from Employee where JobType=\'HR\' and LunchTime=\'12:0:0\';","select * from Employee where Coloumn_Not_Present > 2 limit 2;","select * from Table_not_present where Office > 2 limit 2;","select * from Employee where Salary is null;"]
 create_q = ["CREATE DATABASE mysqltest1;","CREATE USER test1 IDENTIFIED BY 'test1';","GRANT ALL ON mysqltest1.* TO test1;","use mysqltest1","CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT,Name VARCHAR(255),Salary FLOAT,JoinDate DATE,LastLogin DATETIME,LunchTime TIME,OfficeNo TINYINT,JobType ENUM('HR', 'Management', 'Accounts'),Senior BIT(1),empno SMALLINT,PRIMARY KEY (ID));","SET autocommit = 0;","START TRANSACTION WITH CONSISTENT SNAPSHOT;","INSERT INTO Employee (Name, Salary, JoinDate, LastLogin, LunchTime, OfficeNo, JobType, Senior, empno) VALUES ('John', 10000.50, '2015-8-3', '2015-9-5 12:31:30', '12:00:00', 1, 'HR', b'1', 1301), ('Tom', 20000.25, '2015-8-4', '2015-10-12 13:12:14', '13:00:00', 12, 'HR', b'1', 1422), ('Jim', 30000.00, '2015-6-2', '2015-9-5 10:05:10', '12:30:00', 45, 'Management', b'0', 1567), ('Tim', 15000.50, '2015-7-25', '2015-10-10 12:12:25', '12:30:00', 56, 'Accounts', b'1', 3200);","UPDATE Employee SET Salary = 25000.00 WHERE ID > 2;","INSERT INTO Employee VALUES ();","delete from Employee where name is null;"]
 drop_q = ["DROP TABLE Employee;","DROP DATABASE mysqltest1;","DROP user test1;"]
@@ -131,7 +130,6 @@ else
   println("Macro @query failed the test")
 end
 
-#Making
 
 listdsns()
 listdrivers()
@@ -185,6 +183,3 @@ println("Number of failed queries  = $(Query_failed+Query_Error)")
 println("Queries that generated errors = $Query_Error")
 println("Total number of queries executed: $(Query_failed+Query_Error+Query_passed)")
 println("\n******  END OF SUMMARY ******\n\n\n")
-
-
-
