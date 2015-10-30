6b219d8d3663623730f4a0890b8aabcc3c407d1f
bmharsha <bmharshajobs@gmail.com>
2015-10-30 12:10:28 +0530

    Fixed  incorrect  count bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@
 
 DSN = "Default"
 Username = "root"
-Password = "password"
+Password = "root"
 
 
 using DataFrames
@@ -21,12 +21,12 @@ function check_API_query(q,print_file=0,negative_testing=false)
   global query_counter
   global querymeta_counter
   try
+    query_counter  = query_counter  +1
     if print_file == 1
-      query_counter  = query_counter  +1
       ODBC.query(q,output="query$query_counter.csv",delim=':')
     else
       a = ODBC.query(q)
-      if (typeof(a) == DataFrame)
+      if (typeof(a) == DataFrame) && !(negative_testing)
         Query_passed = Query_passed + 1
         #println("API query <$q> passed")
       else
@@ -34,10 +34,10 @@ function check_API_query(q,print_file=0,negative_testing=false)
         Query_failed = Query_failed + 1
       end
     end
-    if(negative_testing)
-      println("\n\nAPI query <$q> failed the negative test\n\n")
-      Query_failed = Query_failed + 1
-    end
+    #if(negative_testing)
+      #println("\n\nAPI query <$q> failed the negative test\n\n")
+      #Query_failed = Query_failed + 1
+    #end
   catch
     if(!negative_testing)
       println("\n\n\nQuery \n<$q>\n is generating an error, please revise your query\n\n\n")
@@ -56,12 +56,12 @@ function check_API_querymeta(q,print_file=0,negative_testing=false)
   global query_counter
   global querymeta_counter
   try
+    querymeta_counter = querymeta_counter +1
     if print_file == 1
-      querymeta_counter = querymeta_counter +1
       ODBC.query(q,output="querymeta$querymeta_counter.csv",delim=':')
     else
       a = ODBC.querymeta(q)
-      if (typeof(a) == Metadata)
+      if (typeof(a) == Metadata) && !(negative_testing)
         #println("API querymeta <$q> passed")
         Query_passed = Query_passed + 1
       else
@@ -69,13 +69,13 @@ function check_API_querymeta(q,print_file=0,negative_testing=false)
         Query_failed = Query_failed + 1
       end
     end
-    if(negative_testing)
-      println("\n\nAPI query <$q> failed the negative test\n\n")
-      Query_failed = Query_failed + 1
-    end
+    #if(negative_testing)
+      #println("\n\nAPI querymeta <$q> failed the negative test\n\n")
+      #Query_failed = Query_failed + 1
+    #end
   catch
     if(!negative_testing)
-      println("\n\n\nQuery \n<$q>\n is generating an error, please revise your query\n\n\n")
+      println("\n\n\nQuery \n<$q>\n is generating an error, please revise your query in querymeta\n\n\n")
       Query_Error = Query_Error + 1
       return
     end
@@ -86,7 +86,7 @@ end
 
 q = ["show databases;", "use mysqltest1;", "show tables;", "select * from Employee;", "select count(*) from Employee;", "show columns from Employee;", "select ID from Employee group by ID;", "select * from Employee where Salary = -1;","select count(*) from Employee where OfficeNo > 0;", "select * from Employee where OfficeNo > 2 limit 2;","select a.ID, b.ID from Employee a left outer join Employee b on a.ID = b.ID where a.OfficeNo > 2 and b.OfficeNo < 45;","select * from Employee where JobType=\'HR\' and LunchTime=\'12:0:0\';","select * from Employee where Salary is null;"]
 create_q = ["CREATE DATABASE mysqltest1;","CREATE USER test1 IDENTIFIED BY 'test1';","GRANT ALL ON mysqltest1.* TO test1;","use mysqltest1","CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT,Name VARCHAR(255),Salary FLOAT,JoinDate DATE,LastLogin DATETIME,LunchTime TIME,OfficeNo TINYINT,JobType ENUM('HR', 'Management', 'Accounts'),Senior BIT(18),empno SMALLINT,PRIMARY KEY (ID));","SET autocommit = 0;","START TRANSACTION WITH CONSISTENT SNAPSHOT;","INSERT INTO Employee (Name, Salary, JoinDate, LastLogin, LunchTime, OfficeNo, JobType, Senior, empno) VALUES ('John', 10000.50, '2015-8-3', '2015-9-5 12:31:30', '12:00:00', 1, 'HR', b'1', 1301), ('Tom', 20000.25, '2015-8-4', '2015-10-12 13:12:14', '13:00:00', 12, 'HR', b'1', 1422), ('Jim', 30000.00, '2015-6-2', '2015-9-5 10:05:10', '12:30:00', 45, 'Management', b'0', 1567), ('Tim', 15000.50, '2015-7-25', '2015-10-10 12:12:25', '12:30:00', 56, 'Accounts', b'1', 3200);","UPDATE Employee SET Salary = 25000.00 WHERE ID > 2;","INSERT INTO Employee VALUES ();","delete from Employee where name is null;"]
-drop_q = ["DROP TABLE Employee;","DROP DATABASE mysqltest1;","DROP user test1;"]
+drop_q = ["DROP TABLE Employee;","DROP DATABASE mysqltest1;","DROP user test1;","FLUSH PRIVILEGES;"]
 
 
 # Testing API Query
@@ -154,7 +154,6 @@ else
   println("\n\nMacro @query failed the test\n\n")
 end
 
-
 listdsns()
 listdrivers()
 ODBC.disconnect(con)
@@ -175,20 +174,33 @@ end
                                           #NEGATIVE TESTING
 
 
-println("\n\n\t\t\t******  NEGATIVE TESTING ******\n\n")
-error_q = ["select count(*) from Employee; select count(*) from Employee;","select * from Employee where Coloumn_Not_Present > 2 limit 2;","select * from Table_not_present where Office > 2 limit 2;","use Inexistent_DataBase;", "CREATE DATABASE mysqltest1;", "CREATE DATABASE mysqltest1;", "CREATE USER test1 IDENTIFIED BY 'test1';", "CREATE USER test1 IDENTIFIED BY 'test1';", "GRANT ALL ON Inexistent_DataBase.* TO test1;", "DROP user test1;", "GRANT ALL ON mysqltest1.* TO test1;", "use mysqltest1","use mysqltest1", "CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT,Name VARCHAR(255),Salary FLOAT,JoinDate DATE,LastLogin DATETIME,LunchTime TIME,OfficeNo TINYINT,JobType ENUM('HR', 'Management', 'Accounts'),Senior BIT(18),empno SMALLINT,PRIMARY KEY (ID));","CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT,Name VARCHAR(255))","CREATE TABLE ERROR(ID I,Name VARCHAR)","INSERT INTO Employee (Name, Salary, JoinDate, LastLogin, LunchTime, OfficeNo, JobType, Senior, empno) VALUES ('John', , '2015-8-3', '2015-9-5 12:31:30', '12:00:00', 1, 'HR', b'1', 1301)","DROP TABLE inexistent;","DROP TABLE Employee;","DROP TABLE Employee;","DROP DATABASE mysqltest1;","DROP DATABASE mysqltest1;"]
+println("\n\n\t\t\t******  NEGATIVE TESTING ******\n\n");
+error_q = ["select count(*) from Employee; select count(*) from Employee;","select * from Employee where Coloumn_Not_Present > 2 limit 2;","select * from Table_not_present where Office > 2 limit 2;","use Inexistent_DataBase;", "CREATE DATABASE mysqltest1;", "CREATE DATABASE mysqltest1;", "CREATE USER test1 IDENTIFIED BY 'test1';", "CREATE USER test1 IDENTIFIED BY 'test1';", "DROP user test1;","FLUSH PRIVILEGES;", "use mysqltest1", "CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT,Name VARCHAR(255),Salary FLOAT,JoinDate DATE,LastLogin DATETIME,LunchTime TIME,OfficeNo TINYINT,JobType ENUM('HR', 'Management', 'Accounts'),Senior BIT(18),empno SMALLINT,PRIMARY KEY (ID));","CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT,Name VARCHAR(255))","CREATE TABLE ERROR(ID I,Name VARCHAR)","INSERT INTO Employee (Name, Salary, JoinDate, LastLogin, LunchTime, OfficeNo, JobType, Senior, empno) VALUES ('John', , '2015-8-3', '2015-9-5 12:31:30', '12:00:00', 1, 'HR', b'1', 1301)","DROP TABLE inexistent;","DROP TABLE Employee;","DROP TABLE Employee;","DROP DATABASE mysqltest1;","DROP DATABASE mysqltest1;"]
+skip_error_q = [5,7,9,10,11,12,17,19]
 con = ODBC.connect(DSN)
-
+error_q_counter=1
 # Testing API Query
 for i in error_q
-  check_API_query(i,0,true)
+  if error_q_counter in skip_error_q
+    check_API_query(i,0)
+  else
+    check_API_query(i,0,true)
+  end
+  error_q_counter = error_q_counter+1
 end
 
+error_q_counter=1
 # Testing API Meta
 for i in error_q
-  check_API_querymeta(i,0,true)
+  if error_q_counter in skip_error_q
+    check_API_querymeta(i,0)
+  else
+    check_API_querymeta(i,0,true)
+  end
+  error_q_counter = error_q_counter+1
 end
 ODBC.disconnect(con)
+
 
 #Testing AdvancedConnect and connect with incorrect credentials
 try
@@ -227,3 +239,8 @@ println("Number of failed queries  = $Query_failed")
 println("Queries that generated errors = $Query_Error")
 println("Total number of queries executed: $(Query_failed+Query_Error+Query_passed)")
 println("\n******  END OF SUMMARY ******\n\n\n")
+
+#Indicates that current build of ODBC.jl has errors in it
+if Query_Error>0 || Query_failed>0
+  exit(1)
+end
