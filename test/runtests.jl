0a16e81abc26fa82e604b1905ae75b633f3027d4
bmharsha <bmharshajobs@gmail.com>
2015-11-04 11:59:40 +0530

    Testing Travis 1

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,8 +13,6 @@ global Query_failed = 0
 global Query_Error = 0
 global query_counter = 0
 global querymeta_counter = 0
- #correct_answers =  , "test1" in a[:User] , "GRANT ALL PRIVILEGES ON `mysqltest1`.* TO 'test1'@'%'" in a[:Grants_for_test1_] ,  "Employee" in a[:Tables_in_mysqltest1] ,
-
 
 function check_correctness(q,a)
   if q == "CREATE DATABASE mysqltest1;"
@@ -93,6 +91,42 @@ function check_correctness(q,a)
       return false
     end
   end
+
+  if q == "UPDATE Employee SET Salary = 25000.00 WHERE ID > 2;"
+    cross_check = ODBC.query("select ID,Salary from Employee WHERE ID > 2;")
+    correct_answer = load("./dataset/update.jld")["update"]
+    try
+      if isequal(cross_check,correct_answer)
+        return true
+      end
+    catch
+      return false
+    end
+  end
+
+  if q == "INSERT INTO Employee VALUES ();"
+    cross_check = ODBC.query("select ID,Salary from Employee WHERE Salary is null;")
+    correct_answer = load("./dataset/insert_null.jld")["insert_null"]
+    try
+      if isequal(cross_check,correct_answer)
+        return true
+      end
+    catch
+      return false
+    end
+  end
+
+  if q == "delete from Employee where name is null;"
+    cross_check = ODBC.query("select ID,Salary from Employee WHERE Salary is null;")
+    try
+      if size(cross_check) == (0,0)
+        return true
+      end
+    catch
+      return false
+    end
+  end
+
   return true
 end
 
@@ -164,7 +198,7 @@ function ODBC_test()
   global Query_Error
   global query_counter
   global querymeta_counter
-  check_API_query("drop database mysqltest1",0,true)
+  #check_API_query("drop database mysqltest1",0,true)
   q = ["use mysqltest1;",  "select * from Employee;", "select count(*) from Employee;", "show columns from Employee;", "select ID from Employee group by ID;", "select * from Employee where Salary = -1;","select count(*) from Employee where OfficeNo > 0;", "select * from Employee where OfficeNo > 2 limit 2;","select a.ID, b.ID from Employee a left outer join Employee b on a.ID = b.ID where a.OfficeNo > 2 and b.OfficeNo < 45;","select * from Employee where JobType=\'HR\' and LunchTime=\'12:0:0\';","select * from Employee where Salary is null;"]
   create_q = ["CREATE DATABASE mysqltest1;","CREATE USER test1 IDENTIFIED BY 'test1';","GRANT ALL ON mysqltest1.* TO test1;","use mysqltest1","CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT,Name VARCHAR(255),Salary FLOAT,JoinDate DATE,LastLogin DATETIME,LunchTime TIME,OfficeNo TINYINT,JobType ENUM('HR', 'Management', 'Accounts'),Senior BIT(18),empno SMALLINT,PRIMARY KEY (ID));","SET autocommit = 0;","START TRANSACTION WITH CONSISTENT SNAPSHOT;","INSERT INTO Employee (Name, Salary, JoinDate, LastLogin, LunchTime, OfficeNo, JobType, Senior, empno) VALUES ('John', 10000.50, '2015-8-3', '2015-9-5 12:31:30', '12:00:00', 1, 'HR', b'1', 1301), ('Tom', 20000.25, '2015-8-4', '2015-10-12 13:12:14', '13:00:00', 12, 'HR', b'1', 1422), ('Jim', 30000.00, '2015-6-2', '2015-9-5 10:05:10', '12:30:00', 45, 'Management', b'0', 1567), ('Tim', 15000.50, '2015-7-25', '2015-10-10 12:12:25', '12:30:00', 56, 'Accounts', b'1', 3200);","UPDATE Employee SET Salary = 25000.00 WHERE ID > 2;","INSERT INTO Employee VALUES ();","delete from Employee where name is null;"]
   drop_q = ["DROP TABLE Employee;","DROP DATABASE mysqltest1;","DROP user test1;","FLUSH PRIVILEGES;"]
