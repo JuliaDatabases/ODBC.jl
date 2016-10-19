3c767bdd1c8aed9fc09ed99da9fb667e74f138c9
quinnj <quinn.jacobd@gmail.com>
2016-10-19 08:45:16 -0600

    Simplify tests for now to only rely on ODBC and DataFrames. Also add test logging

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Base.Test, ODBC, DataStreams, DataFrames, WeakRefStrings, CSV, SQLite, Feather, CategoricalArrays
+using Base.Test, ODBC, DataStreams, DataFrames, WeakRefStrings
 
 @show ODBC.listdrivers()
 @show ODBC.listdsns()
@@ -18,6 +18,7 @@ ODBC.query(dsn, "use mysql")
 data = ODBC.query(dsn, "select table_name from information_schema.tables")
 
 # setup a test database
+println("testing all mysql types...")
 ODBC.execute!(dsn, "drop database if exists testdb")
 ODBC.execute!(dsn, "create database testdb")
 ODBC.execute!(dsn, "use testdb")
@@ -171,6 +172,7 @@ ODBC.execute!(dsn, "insert test1 VALUES
                     )")
 data = ODBC.query(dsn, "select * from test1")
 @test size(data) == (2,27)
+println("passed. testing large query...")
 
 ODBC.execute!(dsn, "drop table if exists test2")
 ODBC.execute!(dsn, """
@@ -198,55 +200,7 @@ df = ODBC.query(dsn, "select * from test2")
 @test size(df) == (70000,7)
 @test df.columns[1].values == [1:70000...]
 @test df.columns[end][1] === Nullable(ODBC.API.SQLTimestamp(2002,1,17,21,32,0,0))
-
-# test exporting test1 to CSV
-source = ODBC.Source(dsn, "select * from test1")
-csv = CSV.Sink("test1.csv")
-Data.stream!(source, csv)
-Data.close!(csv)
-open("test1.csv") do f
-    @test readline(f) == "\"test_bigint\",\"test_bit\",\"test_decimal\",\"test_int\",\"test_numeric\",\"test_smallint\",\"test_mediumint\",\"test_tiny_int\",\"test_float\",\"test_real\",\"test_date\",\"test_datetime\",\"test_timestamp\",\"test_time\",\"test_year\",\"test_char\",\"test_varchar\",\"test_binary\",\"test_varbinary\",\"test_tinyblob\",\"test_blob\",\"test_mediumblob\",\"test_longblob\",\"test_tinytext\",\"test_text\",\"test_mediumtext\",\"test_longtext\"\n"
-    @test readline(f) == "1,1,+1E+0,1,+1E+0,1,1,1,1.2,1.2,2016-01-01,2016-01-01T01:01:01,2016-01-01T01:01:01,01:01:01,2016,\"A\",\"hey there sailor\",UInt8[0x31,0x32],\"\",UInt8[0x68,0x65,0x79,0x20,0x74,0x68,0x65,0x72,0x65,0x20,0x61,0x62,0x72,0x61,0x68,0x61,0x6d],UInt8[0x68,0x65,0x79,0x20,0x74,0x68,0x65,0x72,0x65,0x20,0x62,0x69,0x6c,0x6c],UInt8[0x68,0x65,0x79,0x20,0x74,0x68,0x65,0x72,0x65,0x20,0x63,0x68,0x61,0x72,0x6c,0x69,0x65],UInt8[0x68,0x65,0x79,0x20,0x74,0x68,0x65,0x72,0x65,0x20,0x64,0x61,0x6e],\"hey there ephraim\",\"hey there frank\",\"hey there george\",\"hey there hank\"\n"
-end
-rm("test1.csv")
-
-# test exporting test2 to CSV
-source = ODBC.Source(dsn, "select * from test2")
-csv = CSV.Sink("test2.csv")
-Data.stream!(source, csv)
-Data.close!(csv)
-open("test2.csv") do f
-    @test readline(f) == "\"ID\",\"first_name\",\"last_name\",\"Salary\",\"hourly rate\",\"hireDate\",\"last clockin\"\n"
-    @test readline(f) == "1,\"Lawrence\",\"Powell\",+87217E+0,26.47,2002-04-09,2002-01-17T21:32:00\n"
-end
-
-# test exporting test1 to SQLite
-db = SQLite.DB()
-source = ODBC.Source(dsn, "select * from test1")
-SQLite.load(db, "test1", source)
-
-data = SQLite.query(db, "select * from test1")
-@test size(data) == (2,27)
-@test data.columns[1][1] === Nullable(1)
-@test data.columns[3][1] === Nullable(1.0)
-@test data.columns[11][1] === Nullable(ODBC.API.SQLDate(2016,1,1))
-
-# test exporting test2 to SQLite
-source = ODBC.Source(dsn, "select * from test2")
-SQLite.load(db, "test2", source)
-
-data = SQLite.query(db, "select * from test2")
-@test size(data) == (70000,7)
-@test data.columns[1].values == [1:70000...]
-@test data.columns[end][1] === Nullable(ODBC.API.SQLTimestamp(2002,1,17,21,32,0,0))
-
-# test exporting test2 to Feather
-file = "test2.feather"
-Feather.write(file, ODBC.Source, dsn, "select ID, first_name, last_name, salary + 0E0 as salary, `hourly rate` from test2")
-df = Feather.read(file)
-@test size(df, 1) == size(data, 1)
-@test df.columns[1] == [1:70000...]
-rm("test2.feather")
+println("passed. testing ODBC.Sink...")
 
 ODBC.execute!(dsn, "create table test3 as select * from test2 limit 0")
 ODBC.execute!(dsn, "delete from test3")
@@ -260,6 +214,7 @@ ODBC.load(dsn, "test3", df; append=true)
 df = ODBC.query(dsn, "select * from test3")
 @test size(df) == (200,7)
 @test df.columns[1].values == append!([1:100...], [1:100...])
+println("passed. testing prepared statement...")
 
 stmt = ODBC.prepare(dsn, "insert into test3 values(?,?,?,?,?,?,?)")
 ODBC.execute!(stmt, [101, "Steve", "McQueen", 1.0, 100.0, Date(2016,1,1), DateTime(2016,1,1)])
@@ -286,40 +241,7 @@ df = ODBC.query(dsn, "select * from test3")
 ODBC.execute!(dsn, "drop table if exists test1")
 ODBC.Source(dsn, "drop table if exists test2")
 ODBC.Source(dsn, "drop table if exists test3")
-rm("test2.csv")
 
-# more robust Feather testing
-mysqltypes = Dict(Int32=>"INT", Float64=>"DOUBLE", CategoricalValue{String,Int32}=>"VARCHAR(255)", CategoricalValue{String,Int32}=>"VARCHAR(255)", WeakRefString{UInt8}=>"VARCHAR(255)")
-columndef(name, typ) = "`$name` $(mysqltypes[typ <: Nullable ? eltype(typ) : typ]),"
-
-testnull{T1, T2}(v1::T1, v2::T2) = v1 == v2
-testnull{T1, T2}(v1::Nullable{T1}, v2::Nullable{T2}) = (isnull(v1) && isnull(v2)) || (!isnull(v1) && !isnull(v2) && get(v1) == get(v2))
-testnull{T1, T2}(v1::T1, v2::Nullable{T2}) = !isnull(v2) && get(v2) == v1
-testnull{T1, T2}(v1::Nullable{T1}, v2::T2) = !isnull(v1) && get(v1) == v2
-
-feather_dir = joinpath(dirname(@__FILE__), "../../Feather/test/data")
-# feather_dir = "/Users/jacobquinn/.julia/v0.5/Feather/test/data"
-# files = readdir(feather_dir)
-for file in readdir(feather_dir)
-    source = Feather.Source(joinpath(feather_dir,file))
-    nms = Data.header(source)
-    typs = Data.types(source)
-    columndefs = ""
-    for i = 1:length(nms)
-        columndefs *= columndef(nms[i], typs[i])
-    end
-    createtable = "CREATE TABLE feather_test ($(chop(columndefs)))"
-    ODBC.execute!(dsn, createtable)
-    ODBC.load(dsn, "feather_test", source)
-    df = ODBC.query(dsn, "select * from feather_test")
-    df2 = Feather.read(source)
-    # tests
-    for (c1,c2) in zip(df.columns,df2.columns)
-        for i = 1:length(c1)
-            @test testnull(c1[i], c2[i])
-        end
-    end
-    ODBC.execute!(dsn, "drop table feather_test")
-end
+println("passed.")
 
 ODBC.disconnect!(dsn)
