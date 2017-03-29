b4d0eb5e55365644e1dcc161cfc4ff71c45b0db2
quinnj <quinn.jacobd@gmail.com>
2017-03-29 12:20:06 -0600

    0.6 upates

--- test/runtests.jl
+++ test/runtests.jl
@@ -252,8 +252,8 @@ if typeof(DF[:hiredate]) <: NullableVector
     DF[:lastclockin] = NullableArray(DateTime[isnull(x) ? DateTime() : DateTime(get(x)) for x in DF[:lastclockin]], [isnull(x) for x in DF[:lastclockin]])
     stringdata = join(String[get(x) for x in strings])
     stringdata2 = join(String[get(x) for x in strings2])
-    DF.columns[2] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), stringdata.data)
-    DF.columns[3] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), stringdata2.data)
+    DF.columns[2] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), Vector{UInt8}(stringdata))
+    DF.columns[3] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), Vector{UInt8}(stringdata2))
     ind = ind2 = 1
     for i = 1:size(DF, 1)
         DF.columns[2][i] = Nullable(WeakRefString(pointer(stringdata, ind), length(get(strings[i])), ind))
@@ -270,8 +270,8 @@ else
     DF.columns[7] = NullableArray(DateTime[isna(x) ? DateTime() : DateTime(x) for x in DF[:lastclockin]], [isna(x) for x in DF[:lastclockin]])
     stringdata = join(String[isna(x) ? "" : x for x in strings])
     stringdata2 = join(String[isna(x) ? "" : x for x in strings2])
-    DF.columns[2] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), stringdata.data)
-    DF.columns[3] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), stringdata2.data)
+    DF.columns[2] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), Vector{UInt8}(stringdata))
+    DF.columns[3] = NullableArray{WeakRefString{UInt8},1}(Array(WeakRefString{UInt8}, size(DF, 1)), ones(Bool, size(DF, 1)), Vector{UInt8}(stringdata2))
     ind = ind2 = 1
     for i = 1:size(DF, 1)
         DF.columns[2][i] = Nullable(WeakRefString(pointer(stringdata, ind), length(strings[i]), ind))
@@ -284,10 +284,10 @@ DF2 = deepcopy(DF)
 function sinktodf(df::DataFrame)
     df2 = deepcopy(df)
     if !(eltype(df2.columns[6]) <: Nullable{Date})
-        df2.columns[6] =  map(x->isnull(x) ? Nullable{Date}() : Nullable(Date(get(x))), df2.columns[6])
+        df2.columns[6] =  map(x->isnull(x) ? Nullable{Date}() : Nullable(Date(x)), df2.columns[6])
     end
     if !(eltype(df2.columns[7]) <: Nullable{DateTime})
-        df2.columns[7] =  map(x->isnull(x) ? Nullable{DateTime}() : Nullable(DateTime(get(x))), df2.columns[7])
+        df2.columns[7] =  map(x->isnull(x) ? Nullable{DateTime}() : Nullable(DateTime(x)), df2.columns[7])
     end
     return df2
 end
