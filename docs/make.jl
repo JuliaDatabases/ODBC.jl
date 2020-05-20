96c83132b432895af05be3b364d2db539fc123e9
Jacob Quinn <quinn.jacobd@gmail.com>
2020-05-20 00:20:32 -0600

    Package and docs cleanup

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,11 @@ using Documenter, ODBC
 
 makedocs(
     modules = [ODBC],
-    format = :html,
     sitename = "ODBC.jl",
     pages = ["Home" => "index.md"]
 )
 
 deploydocs(
     repo = "github.com/JuliaDatabases/ODBC.jl.git",
-    target = "build",
-    deps = nothing,
-    make = nothing,
-    julia = "0.5",
-    osname = "linux"
+    target = "build"
 )
