2e2f7836ad08ac8d8ad2525745c41ad86baa6caf
Jacob Quinn <quinn.jacobd@gmail.com>
2021-04-01 14:38:20 -0600

    try another fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,9 @@ rm(tracefile)
 PLUGIN_DIR = joinpath(MariaDB_Connector_C_jll.artifact_dir, "lib", "mariadb", "plugin")
 if Sys.islinux()
     if Int == Int32
-        libpath = "~/mariadb32/lib/libmaodbc.so"
+        libpath = joinpath(expanduser("~"), "mariadb32/lib/libmaodbc.so")
     else
-        libpath = "~/mariadb64/lib/libmaodbc.so"
+        libpath = joinpath(expanduser("~"), "mariadb64/lib/libmaodbc.so")
     end
 elseif Sys.iswindows()
     if Int == Int32
